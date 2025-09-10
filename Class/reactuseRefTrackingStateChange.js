118f0f8772b0ddca8bda869af3e7b3b4b1f8a667
AbuHamse <dirie.a99@gmail.com>
2025-09-10 02:58:05 -0400

    Added comment

--- Class/reactuseRefTrackingStateChange.js
+++ Class/reactuseRefTrackingStateChange.js
@@ -25,3 +25,29 @@ function App() {
 createRoot(document.getElementById('root')).render(
   <App />
 );
+
+
+/*This time we use a combination of useState, useEffect, and useRef 
+to keep track of the previous state.
+
+In the useEffect, we are updating the useRef current value each 
+time the inputValue is updated by entering text into the input field. */
+
+import React from 'react'
+
+const reactuseRefTrackingStateChange = () => {
+    const [inputValue1, setInputValue1] = useState("");
+    const previousInputValue1 = useRef("")
+
+    useEffect(()=>{
+        previousInputValue1.current = inputValue1
+
+
+    }, [inputValue1])
+
+  return (
+    <div>reactuseRefTrackingStateChange</div>
+  )
+}
+
+export default reactuseRefTrackingStateChange
